f81aedf8b4e541dc8a2d10cace68ef48945f3d92
vanessa <vanessa.191010@gmail.com>
2023-05-24 10:37:11 -0300

    24/05

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -152,5 +152,29 @@ function dibujarCirCuad(){
     ctx.fillStyle = "#993385";
     ctx.stroke();
     ctx.fill();
+}
+
+var bandera;
+function dibujar(event){
+    var canvas = document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
+
+    var posX = event.clientX;
+    var posY = event.clientY;
+    console.log(posX,posY);
+    canvas.onmousedown = function (){bandera=true};
+    canvas.onmouseup = function (){bandera=false};
+
+    if (bandera) {
+
+        ctx.fillRect(posX, posY, 5, 5);
+        ctx.fill();
+    }
+
+}
+function limpiarCanvas(){
+    var canvas = document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
+    canvas.width = canvas.width;
 
 }
